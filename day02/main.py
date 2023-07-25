3cc9b41b5751fb3f0e89453a4243f57fdee2d293
MehakKambo <mehakpreetp12@gmail.com>
2023-07-24 18:29:10 -0700

    Update var name

--- day02/main.py
+++ day02/main.py
@@ -3,13 +3,13 @@ print("Welcome to the tip calculator!")
 
 total_amount = float(input("What is the total bill amount?\n$:"))
 tip_percentage = int(input("How much tip would you like to give? \nPercent:"))
-people_count = int(input("How many people to split the bill?\nPeople:"))
+split = int(input("How many people to split the bill?\nPeople:"))
 
 tip_amount = tip_percentage * (total_amount) / 100
 total_amount += tip_amount
 
-if people_count < 2:
+if split < 2:
     print(f"Each person should pay: ${total_amount}")
 else:
-    total_amount = total_amount / people_count
+    total_amount = total_amount / split
     print(f"Each person should pay: ${round(total_amount, 2)}")
